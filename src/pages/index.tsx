a4564b3235723b52a01a03d3e9eb3a387f983c25
radson.lima <radson.lima@grupoicts.com.br>
2022-06-19 15:06:20 -0300

    criação da tabela e habilitação dos botoes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
+import { log } from "console";
+import { useState } from "react";
+import Botao from "../components/Botao";
+import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
@@ -18,6 +22,12 @@ export default function Home() {
     console.log(`Excluir... ${cliente.nome}`)
   }
 
+  function salvarCliente(cliente: Cliente) {
+    console.log(cliente)
+  }
+
+  const [visivel, setVisivel] = useState< 'tabela' | 'form' >('tabela')
+
   return (
     <div className={`
       flex justify-center items-center h-screen
@@ -25,10 +35,33 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
-          <Tabela clientes={clientes}
-            clienteSelecionado={clienteSelecionado}
-            clienteExcluido={clienteExcluido} 
+        { visivel === 'tabela' ? (
+          <>
+            <div className="flex justify-end">
+              <Botao cor='green' 
+              className="
+                mb-4 bg-gradient-to-r 
+                from-green-400 
+                to-green-700"
+                onClick={setVisivel}
+              >
+                Novo Cliente
+              </Botao>
+            </div>
+            <Tabela clientes={clientes}
+              clienteSelecionado={clienteSelecionado}
+              clienteExcluido={clienteExcluido} 
+            />
+          </>
+        ) : (
+          <div className="border rounded-lg bg-gray-200 p-7">
+          <Formulario 
+            cliente={clientes[2]}
+            cancelado={() => setVisivel('tabela')}
+            clienteMudou={salvarCliente} 
           />
+          </div>
+        )}
       </Layout>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}'
-  ],
+  purge:{
+    content: [
+      './src/pages/**/*.{js,ts,jsx,tsx}',
+      './src/components/**/*.{js,ts,jsx,tsx}'
+    ],
+    safelist: [
+      /^bg-/,
+      /^to-/,
+      /^from-/,
+    ]
+  },
   darkMode: false, //or 'media' or 'class'
   theme: {
     extend: {},
